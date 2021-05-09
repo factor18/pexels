ad51a22ae9659dfaf39803c6d3ff35221857a434
Sarat Chandra Balla <sarat1669@gmail.com>
2021-05-10 02:52:45 +0530

    enable coveralls.github

--- mix.exs
+++ mix.exs
@@ -14,6 +14,7 @@ defmodule Pexels.MixProject do
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
         coveralls: :test,
+        "coveralls.github": :test,
         "coveralls.detail": :test,
         "coveralls.post": :test,
         "coveralls.html": :test
